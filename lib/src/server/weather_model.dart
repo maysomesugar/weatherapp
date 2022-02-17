1994074c0c229176470db0a6cf1960eb433f00a8
Tomofeyy <temofeyy.20@gmail.com>
2022-02-17 15:52:52 +0200

    switch => if else

--- lib/src/server/weather_model.dart
+++ lib/src/server/weather_model.dart
@@ -14,18 +14,20 @@ class WeatherModel {
 
   String get mainStatus => _weather?['main'] ?? 'unknown';
   String get description => _weather?['description'] ?? 'unknown';
-  Image get icon => _getIconFromCode(_weather?['icon']  ?? '')['icon'];
+  Image get icon => _getImagesFromCode(_weather?['icon'] ?? '')['icon'];
+  String get weatherBackground =>
+      _getImagesFromCode(_weather?['icon'] ?? '')['background'];
+
   int get temperature => KelvinToCelsium(_main?['temp']).round();
   int get feelsLikeTemperature => KelvinToCelsium(_main?['feels_like']).round();
   int get pressure => _main?['pressure'] ?? 0;
   int get humidity => _main?['humidity'] ?? 0;
 
   double get windSpeed => _wind?['speed'] ?? 0;
-  int get windDegrees => _wind?['deg'] ?? 0 ;
+  int get windDegrees => _wind?['deg'] ?? 0;
 
   String get countryName => _countryName ?? 'unknown location';
   String get cityName => _cityName ?? '';
-  String get weatherBackground => _getIconFromCode(_weather?['icon']  ?? '')['background'];
 
   //constructors
 
@@ -63,65 +65,56 @@ class WeatherModel {
     }
   }
 
-  Map<String, dynamic> _getIconFromCode(String code){
+  Map<String, dynamic> _getImagesFromCode(String code) {
     String imagePath;
     String backgroundPath;
 
-    switch (code) {
-      case '01d':
-        imagePath = 'assets/weather_status/sunny.png';
-        backgroundPath = 'assets/images/clear_sky.jpg';
-        break;
-      case '01n':
-        imagePath = 'assets/weather_status/moon.png';
-        backgroundPath = 'assets/images/night_clear_sky.jpg';
-        break;
-      case '11d':
-        imagePath = 'assets/weather_status/thunderstorm.png';
-        backgroundPath = 'assets/images/thunder.jpg';
-        break;
-      case '09d':
-      case '10d':
-        imagePath = 'assets/weather_status/rainy_day.png';
-        backgroundPath = 'assets/images/rain.jpg';
-        break;
-      case '13d':
-        imagePath = 'assets/weather_status/snow.png';
-        backgroundPath = 'assets/images/snow.jpg';
-        break;
-      case '50d':
-        imagePath = 'assets/weather_status/haze.png';
-        backgroundPath = 'assets/images/haze.jpg';
-        break;
-      case '02d':
-        imagePath = 'assets/weather_status/clear_sky.png';
-        backgroundPath = 'assets/images/clear_sky.jpg';
-        break;
-      case '02n':
-        imagePath = 'assets/weather_status/cloudy_night.png';
-        backgroundPath = 'assets/images/cloudy_night.jpg';
-        break;
-      case '03d':
-      case '03n':
-      case '04d':
-      case '04n':
-        imagePath = 'assets/weather_status/cloud.png';
-        backgroundPath = 'assets/images/cloudy.jpg';
-        break;
-      default:
-        imagePath = 'assets/weather_status/sunny.png';
-        backgroundPath = 'assets/images/clear_sky.jpg';
-        break;
+    if (code == '01d') {
+      imagePath = 'assets/weather_status/sunny.png';
+      backgroundPath = 'assets/images/clear_sky.jpg';
+    } else if (code == '01n') {
+      imagePath = 'assets/weather_status/moon.png';
+      backgroundPath = 'assets/images/night_clear_sky.jpg';
+    } else if (code == '11d') {
+      imagePath = 'assets/weather_status/thunderstorm.png';
+      backgroundPath = 'assets/images/thunder.jpg';
+    } else if (code == '09d' || code == '10d') {
+      imagePath = 'assets/weather_status/rainy_day.png';
+      backgroundPath = 'assets/images/rain.jpg';
+    } else if (code == '13d') {
+      imagePath = 'assets/weather_status/snow.png';
+      backgroundPath = 'assets/images/snow.jpg';
+    } else if (code == '50d') {
+      imagePath = 'assets/weather_status/haze.png';
+      backgroundPath = 'assets/images/haze.jpg';
+    } else if (code == '02d') {
+      imagePath = 'assets/weather_status/clear_sky.png';
+      backgroundPath = 'assets/images/clear_sky.jpg';
+    } else if (code == '02n') {
+      imagePath = 'assets/weather_status/cloudy_night.png';
+      backgroundPath = 'assets/images/cloudy_night.jpg';
+    } else if (code == '03d' ||
+        code == '03n' ||
+        code == '04d' ||
+        code == '04n') {
+      imagePath = 'assets/weather_status/cloud.png';
+      backgroundPath = 'assets/images/cloudy.jpg';
+    } else {
+      imagePath = 'assets/weather_status/sunny.png';
+      backgroundPath = 'assets/images/clear_sky.jpg';
     }
 
     return <String, dynamic>{
-      'icon': Image.asset(imagePath,width: 75, color: Colors.black.withOpacity(0.8)),
+      'icon': Image.asset(imagePath,
+          width: 75, color: Colors.black.withOpacity(0.8)),
       'background': backgroundPath
     };
   }
 
   @override
-  String toString(){
+  String toString() {
     return 'temp = $temperature, status = $description';
   }
 }
+
+
